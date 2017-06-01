12434f05263a1b20366c394911db9c9b0c3dce49
arajasek <arajasek@scspcxxx.cs.uwaterloo.ca>
2017-06-01 19:56:21 -0400

    Update State.h

--- State.h
+++ State.h
@@ -2,11 +2,18 @@
 #define STATE_H
 
 #include <string>
+#include <unordered_map>
+
+typedef std::pair<unsigned int, char> transition;
+
 
 class State {
 	unsigned int id;
 	bool final;
 	std::string name;
+
+	std::unordered_map <char, transition> transitions;
+
 public:
 	unsigned int getId() {return id;}
 	void setId (unsigned int i) {id = i;}
@@ -15,6 +22,8 @@ public:
 	void setFinal(bool f) {final = f;}
 	std::string getName() {return name;}
 	void setName(std::string n) {name = n;}
+
+	void addTransition(char c, transition t) { transitions.insert(std::pair<char, transition>(c,t));}
 };
 
 #endif
