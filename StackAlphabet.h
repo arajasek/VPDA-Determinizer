083ba717ef317849bc3a1b5bee41c92cdfa9aea2
arajasek <arajasek@scspcxxx.cs.uwaterloo.ca>
2017-06-01 19:18:04 -0400

    Create StackALphabet

---
+++ StackAlphabet.h
@@ -0,0 +1,17 @@
+#ifndef STACK_ALPHABET_H
+#define STACK_ALPHABET_H
+
+#include <map>
+
+enum SymbolType {PUSH, LOCAL, POP};
+
+class StackAlphabet {
+
+	std::map <char, SymbolType> symbols;
+public:
+	void addSymbol(char s, SymbolType t) { symbols.insert(std::pair <char,SymbolType> (s,t));}
+	void removeSymbol(char s) {symbols.erase(symbols.find(s));}
+	SymbolType getSymbolType(char s) {return symbols.at(s);}
+};
+
+#endif
